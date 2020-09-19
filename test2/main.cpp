137635043d1acccd3cba1f2d4c2a335fb0426fb9
xpk20040228 <xpk20040228@gmail.com>
2020-09-19 12:28:05 +0800

    namespacestd

--- test2/main.cpp
+++ test2/main.cpp
@@ -5,19 +5,19 @@ using namespace std;
 int main()
 {
     int int_NumOfLang= -1;
-    std::cout<<"Hello reader.\n"
+    cout<<"Hello reader.\n"
     <<"Wellcome to C++.\n";
 
-    std::cout<<"How many languages have you used?";
-    std::cin>>int_NumOfLang;
+    cout<<"How many languages have you used?";
+    cin>>int_NumOfLang;
 
     if(int_NumOfLang<1)
     {
-        std::cout<<"None?"<<"Are your serious?\n";12
+        cout<<"None?"<<"Are your serious?\n";12
     }
     else
     {
-        std::cout<<"Enjoy the book.\n";
+        cout<<"Enjoy the book.\n";
     }
     return 0;
 }
